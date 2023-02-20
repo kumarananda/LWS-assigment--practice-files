d8cc555f5f8fdcb84cbcb9330156edd6c90cd8eb
kumarananda <coderanandakumar@gmail.com>
2023-02-20 14:13:40 +0600

    up

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
+const allMatches = document.querySelector('.all-matches')
+
 // initial state
-const initialState = [{
-  matchcount:0
-}]
+const initialState = {
+  matchcount:[0,2]
+}
 
 // create reducer function
-function matchReducer(state = initialState, action){
-  if(action.type === 'increment'){
+function matchReducer(state = initialState, {type, value, index}){
+  if(type === 'increment'){
+
     return {
       ...state,
-      matchcount : state.matchcount + action.payload
+      matchcount : ''
+
 
     }
-  }else if(action.type === 'decrement'){
+  }else if(type === 'decrement'){
+
     return {
       ...state,
-      matchcount : state.matchcount - action.payload
+      matchcount : ''
 
     }
   }else {
@@ -24,9 +29,90 @@ function matchReducer(state = initialState, action){
 
 // create redux store
 const store = Redux.createStore(matchReducer)
+const rander =()=> {
+  const state = store.getState()
+  console.log(state);
+  allMatches.innerHTML = allMatch(state.matchcount) // ?
+}
+
+rander()
+
+store.subscribe(rander)
 
 
 const testArray = [{id:1, name: 'match 1'},{id:2, name: "match 2"}]
+
+
+
+function search(ele, index) {
+  if(event.keyCode == 13) {
+    event.preventDefault()
+
+    store.dispatch({
+      type : ele.name,
+      payload : {
+        index: index,
+        value : Number(ele.value)
+      }
+    })
+    // console.log(index);
+
+    // if(ele.name === 'increment'){
+
+    // }else if(ele.name === 'decrement'){
+
+    // }
+    
+       
+  }
+}
+
+
+
+
+
+function allMatch(array) {
+
+  let matchHTML = "";
+  array.forEach(element => {
+    
+  });
+
+  array.forEach((item, index)=> {
+
+    matchHTML += `
+    <div class="match">
+          <div class="wrapper">
+            <button class="lws-delete">
+              <img src="./image/delete.svg" alt="" />
+            </button>
+            <h3 class="lws-matchName">Match 1</h3>
+          </div>
+          <div class="inc-dec">
+            <form class="incrementForm">
+              <h4>Increment</h4>
+              <input type="number" name="increment" class="lws-increment" onkeydown="search(this, ${index})" />
+            </form>
+            <form class="decrementForm">
+              <h4>Decrement</h4>
+              <input type="number" name="decrement" class="lws-decrement" onkeydown="search(this, ${index})" />
+            </form>
+          </div>
+          <div class="numbers">
+            <h2 class="lws-singleResult">${item}</h2>
+          </div>
+        </div>
+
+    `
+  })
+  return matchHTML    
+
+}
+// allMatch()
+
+
+
+
 
 // // get elements
 // const lwsDelete = document.querySelector('.lws-delete')
@@ -34,51 +120,3 @@ const testArray = [{id:1, name: 'match 1'},{id:2, name: "match 2"}]
 // const lwsIncrement = document.querySelector('.lws-increment')
 // const lwsDecrement = document.querySelector('.lws-decrement')
 // const lwsSingleResult = document.querySelector('.lws-singleResult')
-const allMatches = document.querySelector('.all-matches')
-
-const testAlert = (index)=> {
-  alert(index)
-}
-const testinput = (index)=> {
-  if(event.key === 'Enter') {
-    alert(index.value);        
-}
-}
-
-
-
-
-
-function allMatch() {
-  allMatches;
-  let matchHTML = "";
-
-  testArray.map((item, index)=> {
-    matchHTML += `
-      <div class="match">
-        <div class="wrapper">
-          <button onclick="testAlert(${index})"  class="lws-delete">
-            <img src="./image/delete.svg" alt="" />
-          </button>
-          <h3 class="lws-matchName">${"Match " + (index+1 )}</h3>
-        </div>
-        <div class="inc-dec">
-          <form class="incrementForm">
-            <h4>Increment</h4>
-            <input type="number" name="increment" class="lws-increment" />
-          </form>
-          <form class="decrementForm">
-            <h4>Decrement</h4>
-            <input type="number" name="decrement" class="lws-decrement" onkeydown="testinput(${index})/>
-          </form>
-        </div>
-        <div class="numbers">
-          <h2 class="lws-singleResult">120</h2>
-        </div>
-      </div>
-    `
-  })
-  allMatches.innerHTML = matchHTML    
-
-}
-allMatch()
