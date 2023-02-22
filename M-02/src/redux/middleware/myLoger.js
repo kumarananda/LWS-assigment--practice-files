17dc1879b49357cd1b8e3107ce346f83eef6b6a2
“kumarananda” <coderanandakumar@gmail.com>
2023-02-23 01:29:54 +0600

    custom middleware

---
+++ M-02/src/redux/middleware/myLoger.js
@@ -0,0 +1,21 @@
+import rootReducer from "../rootReducer";
+
+
+
+// create middleware
+const myLoger = (store) => (next) => (action) => {
+
+    console.log("Redux data flow");
+    console.log(`Action: ${JSON.stringify(action)}`);
+    console.log(`Before: ${JSON.stringify(store.getState())}`);
+
+    const upcomingState = [action].reduce(rootReducer, store.getState());
+
+    console.log(`Upcoming State: ${JSON.stringify(upcomingState)}`);
+
+    // pass action
+    return next(action)
+
+}
+
+export default myLoger;
