2596aee8f8cbaa7c5c379dc3150decce4a48f40e
kumarananda <coderanandakumar@gmail.com>
2023-02-27 17:17:01 +0600

    M-04 html to react

--- M-04/src/App.js
+++ M-04/src/App.js
@@ -1,11 +1,27 @@
 
 import './App.css';
+import Header from './components/Header';
+import AddEditForm from './components/AddEditForm';
+import BookList from './components/BookList';
 
 function App() {
   return (
     <>
-      
-    </>
+    <Header/>
+
+    <main className="py-12 2xl:px-6">
+      <div className="container grid xl:grid-cols-[auto_350px] 2xl:grid-cols-[auto_400px] gap-4 2xl:gap-8">
+        <div className="order-2 xl:-order-1">
+          <BookList/>
+          
+        </div>
+        <div>
+          <AddEditForm/>
+
+        </div>
+      </div>
+    </main>
+  </>
   );
 }
 
