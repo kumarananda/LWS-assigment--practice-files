34c4e6917f9c803e175b2e17c04f7cd1df66fa73
“kumarananda” <coderanandakumar@gmail.com>
2023-03-11 22:35:20 +0600

    html to react done

---
+++ m-07/src/components/JobList/JobList.jsx
@@ -0,0 +1,16 @@
+/** @format */
+
+import React from "react";
+import Job from "../Job/Job";
+
+function JobList() {
+  return (
+    <>
+      <div className="jobs-list">
+        <Job />
+      </div>
+    </>
+  );
+}
+
+export default JobList;
