30f31dbc9bf8da0459a08cb39e9aa47cdf1df4a2
“kumarananda” <coderanandakumar@gmail.com>
2023-03-29 01:43:57 +0600

    react done

--- m-final/src/pages/admin/AdminLogin/AdminLogin.jsx
+++ m-final/src/pages/admin/AdminLogin/AdminLogin.jsx
@@ -1,9 +1,23 @@
 /** @format */
 
 import React from "react";
+import learningportalImg from "../../../assets/image/learningportal.svg";
+import AdminLoginForm from "../../../components/AdminLoginForm/AdminLoginForm";
 
 const AdminLogin = () => {
-  return <></>;
+  return (
+    <>
+      <section className="py-6 bg-primary h-screen grid place-items-center">
+        <div className="mx-auto max-w-md px-5 lg:px-0">
+          <div>
+            <img className="h-12 mx-auto" src={learningportalImg} alt="Learning PortalImg" />
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-slate-100">Sign in to Admin Account</h2>
+          </div>
+          <AdminLoginForm />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default AdminLogin;
--- m-final/src/pages/student/StudentRegistration/StudentRegistration.jsx
+++ m-final/src/pages/student/StudentRegistration/StudentRegistration.jsx
@@ -2,22 +2,22 @@
 
 import React from "react";
 import learningportalImg from "../../../assets/image/learningportal.svg";
-import StudentLoginForm from "../../../components/StudentLoginForm/StudentLoginForm";
+import StudentRegForm from "../../../components/StudentRegForm/StudentRegForm";
 
-const StudentLogin = () => {
+const StudentRegistration = () => {
   return (
     <>
       <section className="py-6 bg-primary h-screen grid place-items-center">
         <div className="mx-auto max-w-md px-5 lg:px-0">
           <div>
             <img className="h-12 mx-auto" src={learningportalImg} alt="Learning Portal" />
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-slate-100">Sign in to Student Account</h2>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-slate-100">Create Your New Account</h2>
           </div>
-          <StudentLoginForm />
+          <StudentRegForm />
         </div>
       </section>
     </>
   );
 };
 
-export default StudentLogin;
+export default StudentRegistration;
